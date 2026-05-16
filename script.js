1b7a76f53344fb379bc794c31231ae19f845c309
phuongkim98 <128014314+phuongkim98@users.noreply.github.com>
2026-05-16 13:18:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -268,3 +268,25 @@ window.addEventListener(
     'DOMContentLoaded',
     startStorySlider
 );
+// STORY SLIDER
+function startStorySlider() {
+
+    const slides = document.querySelectorAll('.story-slide');
+
+    if (slides.length === 0) return;
+
+    let current = 0;
+
+    setInterval(() => {
+
+        slides[current].classList.remove('active');
+
+        current = (current + 1) % slides.length;
+
+        slides[current].classList.add('active');
+
+    }, 3000);
+
+}
+
+startStorySlider();
